e6bd8d78bead5306fa6c506d5911e79a0685c528
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2024-02-18 23:55:10 +0530

    create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,14 @@ class ArticlesController < ApplicationController
       @articles = Article.all  
     end
 
+    def new
+    #   @articles = Article.all  
+    end
+
+    def create
+        @articles = Article.new(params.require(:article).permit(:title, :description))
+        @articles.save
+        redirect_to @articles
+    end
+
 end
